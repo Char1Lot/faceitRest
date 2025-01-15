5d70fb052663ddc57dcd5eda03e4eb1c1514427b
Denis Elistratov <deniska2003elistratov@mail.ru>
2025-01-15 18:52:32 +0300

    Добавил геттеры\сеттеры, добавил конструкторы без аргументов и с аргументами, переопределил toString() для User

--- src/main/java/ru/chariot/faceitparser/model/User.java
+++ src/main/java/ru/chariot/faceitparser/model/User.java
@@ -15,4 +15,39 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<ParsedData> parsedData;
+
+    public User(String name, List<ParsedData> parsedData) {
+        this.name = name;
+        this.parsedData = parsedData;
+    }
+
+    public User(){};
+
+    @Override
+    public String toString() {
+        return "{"+
+                "\nuser : " + id +
+                "\nname : " + name +
+                "\n}";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ParsedData> getParsedData() {
+        return parsedData;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setParsedData(List<ParsedData> parsedData) {
+        this.parsedData = parsedData;
+    }
 }
