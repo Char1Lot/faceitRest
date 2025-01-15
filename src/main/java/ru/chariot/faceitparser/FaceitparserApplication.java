16d61f0b6180c548dd595a51d8d4f449789fd418
Denis Elistratov <deniska2003elistratov@mail.ru>
2025-01-15 18:19:15 +0300

    First commit el classic

---
+++ src/main/java/ru/chariot/faceitparser/FaceitparserApplication.java
@@ -0,0 +1,13 @@
+package ru.chariot.faceitparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceitparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceitparserApplication.class, args);
+	}
+
+}
