47be8c3b1bd76addc425cf2b3dddd3b5270188db
Denis Elistratov <deniska2003elistratov@mail.ru>
2025-01-15 19:32:42 +0300

    Добавил метод поиска всех записей, относящихся к пользователю, так же добавил метод поиска всех записей по определенной строке

--- src/main/java/ru/chariot/faceitparser/repository/ParsedDataRepository.java
+++ src/main/java/ru/chariot/faceitparser/repository/ParsedDataRepository.java
@@ -3,7 +3,11 @@ package ru.chariot.faceitparser.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.chariot.faceitparser.model.ParsedData;
+import ru.chariot.faceitparser.model.User;
+import java.util.List;
 
 @Repository
 public interface ParsedDataRepository extends JpaRepository<ParsedData, Long> {
+    List<ParsedData> findByUser(User user);
+    List<ParsedData> findByMap(String map);
 }
