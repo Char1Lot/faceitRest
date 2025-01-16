99fd8be291feda84805ec544b95d6b646d3e3d1e
Denis Elistratov <deniska2003elistratov@mail.ru>
2025-01-16 16:26:22 +0300

    Реализовал контроллер для пользователя со всему CRUD операциями

---
+++ src/main/java/ru/chariot/faceitparser/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.chariot.faceitparser.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.chariot.faceitparser.model.User;
+import ru.chariot.faceitparser.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/allusers")
+    public List<User> getAllUsers(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Long id){
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User saveUser(User user){
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User putUser(@PathVariable("id") Long id, User user){
+        return userService.updateUser(id,user);
+    }
+
+    @DeleteMapping("/{id}")
+    public User deleteUserById(@PathVariable("id") Long id){
+        return userService.deleteUser(id);
+    }
+}
